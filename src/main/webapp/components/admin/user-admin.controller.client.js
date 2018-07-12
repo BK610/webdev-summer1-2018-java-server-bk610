e78a5f84c260b1ac019738bff3100b54738fa690
Bailey Kane <bailey.orion.kane@gmail.com>
2018-07-12 16:14:33 -0400

    Changed setting role

--- src/main/webapp/components/admin/user-admin.controller.client.js
+++ src/main/webapp/components/admin/user-admin.controller.client.js
@@ -1,23 +1,94 @@
 (function () {
 
   var userServiceClient = new UserServiceClient();
+  var usernameFld = $('#usernameFld');
+  var passwordFld = $('#passwordFld');
+  var firstNameFld = $('#firstNameFld');
+  var lastNameFld = $('#lastNameFld');
+  var emailFld = $('#emailFld');
+  var roleFld = $('#roleFld');
 
-  function init() {
-    userServiceClient
-      .findAllUsers()
-      .then(renderUsers);
+  function init() {	
+	var updateBtn = $('#updateBtn');
+	updateBtn.click(updateUser);
+	
+	var createBtn = $('#createBtn');
+	createBtn.click(createUser);
+	
+	userServiceClient
+	  .findAllUsers()
+	  .then(renderUsers);
   }
   init();
 
   function renderUsers(users) {
-    console.log(users);
+	console.log(users);
 
     var tbody = $('tbody');
     tbody.empty();
-    for(var i=0; i<users.length; i++) {
-      var user = users[i];
+    for(var i=0; i<users.length; i++) {    	
+      tbody.append(renderUser(users[i]));
+    }
+  }
 
-      var tr = $('<tr>');
+  function deleteUser(event) {
+    console.log(event);
+    var $button = $(event.currentTarget);
+    var id = $button.attr('id');
+
+    userServiceClient
+      .deleteUser(id)
+      .then(function () {
+        userServiceClient
+          .findAllUsers()
+          .then(renderUsers);
+      });
+  }
+  
+  function createUser(event) {
+    console.log(event);
+    var $button = $(event.currentTarget);
+    
+    var newUser = new User();
+    newUser.setUsername(usernameFld.val());
+    newUser.setPassword(passwordFld.val());
+    newUser.setFirstName(firstNameFld.val());
+    newUser.setLastName(lastNameFld.val());
+    newUser.setEmail(emailFld.val());
+    newUser.setRole(roleFld.val());
+    
+    userServiceClient
+      .createUser(newUser)
+      .then(function () {
+        userServiceClient
+          .findAllUsers()
+          .then(renderUsers);
+      });
+  }
+  
+  function updateUser(event) {
+	console.log(event);
+    var $button = $(event.currentTarget);
+    
+    var newUser = new User();
+    newUser.setUsername(usernameFld.val());
+    newUser.setPassword(passwordFld.val());
+    newUser.setFirstName(firstNameFld.val());
+    newUser.setLastName(lastNameFld.val());
+    newUser.setEmail(emailFld.val());
+    newUser.setRole(roleFld.val());
+    
+    userServiceClient
+      .updateUser(newUser)
+      .then(function () {
+        userServiceClient
+          .findAllUsers()
+          .then(renderUsers);
+      });
+  }
+  
+  function renderUser(user) {
+	  var tr = $('<tr>');
       var td = $('<td>');
       td.append(user.username);
       tr.append(td);
@@ -35,35 +106,22 @@
       tr.append(td);
 
       td = $('<td>');
-      td.append('hello@world.com');
+      td.append(user.email);
       tr.append(td);
 
       td = $('<td>');
-      td.append('Student');
+      td.append(user.role);
       tr.append(td);
 
       td = $('<td>');
       var deleteBtn = $('<button>Delete</button>');
       deleteBtn.click(deleteUser);
       deleteBtn.attr('id', user.id);
+      console.log(deleteBtn);
       td.append(deleteBtn);
+      
       tr.append(td);
-
-      tr.appendTo(tbody);
-    }
-  }
-
-  function deleteUser(event) {
-    console.log(event);
-    var $button = $(event.currentTarget);
-    var id = $button.attr('id');
-
-    userServiceClient
-      .deleteUser(id)
-      .then(function () {
-        userServiceClient
-          .findAllUsers()
-          .then(renderUsers);
-      });
+      
+      return tr;
   }
 })();
