64319027ee316c0bd42a942b4810dfb8928b2dd5
Bailey Kane <bailey.orion.kane@gmail.com>
2018-07-12 19:41:59 -0400

    Restructured functionality of update button. First attempt

--- src/main/webapp/components/admin/user-admin.controller.client.js
+++ src/main/webapp/components/admin/user-admin.controller.client.js
@@ -7,6 +7,7 @@
   var lastNameFld = $('#lastNameFld');
   var emailFld = $('#emailFld');
   var roleFld = $('#roleFld');
+  var inEdit = false;
 
   function init() {	
 	var createBtn = $('#createBtn');
@@ -68,54 +69,63 @@
   function updateUser(event) {
 	console.log(event);
     var $button = $(event.currentTarget);
-    var id = $button.attr('id');
+    var row = $button.parent().parent();
     
-    var newUser = new User();
-    newUser.setUsername(usernameFld.val());
-    newUser.setPassword(passwordFld.val());
-    newUser.setFirstName(firstNameFld.val());
-    newUser.setLastName(lastNameFld.val());
-    newUser.setEmail(emailFld.val());
-    newUser.setRole(roleFld.val());
-    
-    emptyFields();
-    
-    userServiceClient
-      .updateUser(id, newUser)
-      .then(function () {
+    if (inEdit) {
+    	var id = $button.attr('id');
+    	var newUser = new User();
+        newUser.setUsername($('.username').val());
+        newUser.setPassword($('.password').val());
+        newUser.setFirstName($('.firstName').val());
+        newUser.setLastName($('.lastName').val());
+        newUser.setEmail($('.email').val());
+        newUser.setRole($('.role').val());
+        
         userServiceClient
-          .findAllUsers()
-          .then(renderUsers);
-      });
+          .updateUser(id, newUser)
+          .then(function () {
+            userServiceClient
+              .findAllUsers()
+              .then(renderUsers);
+          });
+    } else {
+    	inEdit = true;
+    	
+    	row.find('td').each(function () {
+    		this.contentEditable = true;
+    	});
+    	
+    	$button.val('Submit');
+    }
   }
   
   function renderUser(user) {
-	  var tr = $('<tr>');
-      var td = $('<td>');
+	  var tr = $('<tr class=\'row\'>');
+      var td = $('<td class=\'username\'>');
       td.append(user.username);
       tr.append(td);
 
-      td = $('<td>');
+      td = $('<td class=\'password\'>');
       td.append('*******');
       tr.append(td);
 
-      td = $('<td>');
+      td = $('<td class=\'firstName\'>');
       td.append(user.firstName);
       tr.append(td);
 
-      td = $('<td>');
+      td = $('<td class=\'lastName\'>');
       td.append(user.lastName);
       tr.append(td);
 
-      td = $('<td>');
+      td = $('<td class=\'email\'>');
       td.append(user.email);
       tr.append(td);
 
-      td = $('<td>');
+      td = $('<td class=\'role\'>');
       td.append(user.role);
       tr.append(td);
 
-      td = $('<td>');
+      td = $('<td class=\'buttons\'>');
       var deleteBtn = $('<button class=\'btn btn-primary\'>Delete</button>');
       deleteBtn.click(deleteUser);
       deleteBtn.attr('id', user.id);
