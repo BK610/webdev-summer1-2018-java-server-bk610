85c8c26032486f7ae456d94726840af538bc9bb0
Bailey Kane <bailey.orion.kane@gmail.com>
2018-07-12 10:28:24 -0400

    Fix IDs for input fields

--- src/main/webapp/components/admin/user-admin.controller.client.js
+++ src/main/webapp/components/admin/user-admin.controller.client.js
@@ -1,11 +1,11 @@
 (function () {
 
   var userServiceClient = new UserServiceClient();
-  var usernameFld = $('usernameFld');
-  var passwordFld = $('passwordFld');
-  var firstNameFld = $('firstNameFld');
-  var lastNameFld = $('lastNameFld');
-  var emailFld = $('emailFld');
+  var usernameFld = $('#usernameFld');
+  var passwordFld = $('#passwordFld');
+  var firstNameFld = $('#firstNameFld');
+  var lastNameFld = $('#lastNameFld');
+  var emailFld = $('#emailFld');
 //	var roleFld = $('<th>Role</th>');
 
   function init() {	
