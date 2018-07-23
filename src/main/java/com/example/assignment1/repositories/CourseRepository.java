1204343a8f4d69c10b0aae17484d9dd59a6c9ba2
Bailey Kane <bailey.orion.kane@gmail.com>
2018-07-23 13:47:35 -0500

    Adding functionality?

--- src/main/java/com/example/assignment1/repositories/CourseRepository.java
+++ src/main/java/com/example/assignment1/repositories/CourseRepository.java
@@ -1,9 +1,11 @@
 package com.example.assignment1.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.assignment1.models.Course;
 
 public interface CourseRepository extends CrudRepository<Course, Integer> {
-
+	@Query("SELECT course FROM Course course")
+	public Iterable<Course> findAll();
 }
