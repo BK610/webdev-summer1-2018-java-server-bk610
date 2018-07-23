4a3672f6a4a2b6d2c3f831fb395bf547e22dcf85
Bailey Kane <bailey.orion.kane@gmail.com>
2018-07-23 11:38:27 -0500

    Added CourseRepository content

---
+++ src/main/java/com/example/assignment1/repositories/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.example.assignment1.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.webdevsummer22018serverjavajannunzi.models.Course;
+
+public interface CourseRepository extends CrudRepository<Course, Integer> {
+
+}
