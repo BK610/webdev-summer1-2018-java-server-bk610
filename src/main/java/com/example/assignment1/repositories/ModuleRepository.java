384240e72336270b36510ca4b541fa437e00776c
BK610 <bailey.orion.kane@gmail.com>
2018-08-02 22:02:53 -0400

    Added content from prof. github

---
+++ src/main/java/com/example/assignment1/repositories/ModuleRepository.java
@@ -0,0 +1,9 @@
+package com.example.assignment1.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.assignment1.models.Module;
+
+public interface ModuleRepository extends CrudRepository<Module, Integer>{
+
+}
