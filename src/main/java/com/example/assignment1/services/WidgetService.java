510b8b4f4596c4d4a7b734560f2e4d75c19ee3ac
BK610 <bailey.orion.kane@gmail.com>
2018-08-14 22:42:34 -0400

    Add getting widgets

--- src/main/java/com/example/assignment1/services/WidgetService.java
+++ src/main/java/com/example/assignment1/services/WidgetService.java
@@ -8,12 +8,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.assignment1.models.Widget;
+import com.example.assignment1.repositories.WidgetRepository;
 
 @RestController
 @CrossOrigin(origins = "*")
 public class WidgetService {
+	@Autowired
+	WidgetRepository widgetRepository;
+
+	@GetMapping("/api/widget/")
+	public List<Widget> findAllWidgets() {
+		return (List<Widget>) widgetRepository.findAll();
+	}
+
 	@PostMapping("/api/widget")
 	public List<Widget> saveWidgets(@RequestBody List<Widget> widgets) {
 		return widgets;
-	}	
+	}
 }
